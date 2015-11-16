507c2df6b6d616c701a4285b98e8e210754640b2
Gautam Madaan <gautam.madaan@gmail.com>
2015-11-16 02:14:49 -0800

    Route file created

---
+++ app/routes.js
@@ -0,0 +1,90 @@
+var jwt    = require('jsonwebtoken') // used to create, sign, and verify tokens
+
+//==================================================================
+// Models
+//==================================================================
+var User   = require('./models/user'); // get our mongoose model
+//==================================================================
+// controllers
+//==================================================================
+var userController = require('./controller/user')
+,   tagController = require('./controller/tag');
+
+module.exports = function(app, express) {
+
+    // ---------------------------------------------------------
+    // authentication (no middleware necessary since this isnt authenticated)
+    // ---------------------------------------------------------
+
+    // http://localhost:8080/authenticate
+    app.post('/authenticate', userController.signin);
+
+    app.post('/signup', userController.signup);
+
+    // basic route (http://localhost:8080)
+    app.get('/', function(req, res) {
+        res.send('Hello! The API is at http://localhost:' + port + '/api');
+    });
+
+    // ---------------------------------------------------------
+    // get an instance of the router for api routes
+    // ---------------------------------------------------------
+    var apiRoutes = express.Router(); 
+
+    // ---------------------------------------------------------
+    // route middleware to authenticate and check token
+    // ---------------------------------------------------------
+    apiRoutes.use(function(req, res, next) {
+
+        // check header or url parameters or post parameters for token
+        var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+
+        // decode token
+        if (token) {
+
+            // verifies secret and checks exp
+            jwt.verify(token, app.get('superSecret'), function(err, decoded) {          
+                if (err) {
+                    return res.json({ success: false, message: 'Failed to authenticate token.' });      
+                } else {
+                    // if everything is good, save to request for use in other routes
+                    req.decoded = decoded;  
+                    next();
+                }
+            });
+
+        } else {
+            // if there is no token
+            // return an error
+            return res.status(403).send({ 
+                success: false, 
+                message: 'No token provided.'
+            });
+            
+        }
+        
+    });
+
+    // ---------------------------------------------------------
+    // authenticated routes
+    // ---------------------------------------------------------
+
+    apiRoutes.get('/', function(req, res) {
+        res.json({ message: 'Welcome to the coolest API on earth!' });
+    });
+
+    // Remove this, should never be exposed
+    apiRoutes.get('/users', function(req, res) {
+        User.find({}, function(err, users) {
+            res.json(users);
+        });
+    });
+
+    app.get('/tags', tagController.getTags);
+
+    apiRoutes.get('/check', function(req, res) {
+        res.json(req.decoded);
+    });
+
+    app.use('/api', apiRoutes);
+}
--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 // =================================================================
 // get the packages we need ========================================
 // =================================================================
-var express 	= require('express');
-var app         = express();
-var bodyParser  = require('body-parser');
-var morgan      = require('morgan');
-var mongoose    = require('mongoose');
+var express 	= require('express')
+,	app         = express()
+,	bodyParser  = require('body-parser')
+,	morgan      = require('morgan')
+,	mongoose    = require('mongoose')
+,	config = require('./config'); // get our config file
 
-var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
-var config = require('./config'); // get our config file
-var User   = require('./app/models/user'); // get our mongoose model
-//==================================================================
-// controllers
-//==================================================================
-var userController = require('./app/controller/user');
-var tagController = require('./app/controller/tag');
+
 // =================================================================
 // configuration ===================================================
 // =================================================================
@@ -33,85 +27,7 @@ app.use(morgan('dev'));
 // routes ==========================================================
 // =================================================================
 
-app.post('/signup', userController.signup);
-app.get('/users', function(req, res) {
-	User.find({}, function(err, users) {
-		res.json(users);
-	});
-});
-
-// basic route (http://localhost:8080)
-app.get('/', function(req, res) {
-	res.send('Hello! The API is at http://localhost:' + port + '/api');
-});
-
-app.get('/tags', tagController.getTags);
-
-// ---------------------------------------------------------
-// get an instance of the router for api routes
-// ---------------------------------------------------------
-var apiRoutes = express.Router(); 
-
-// ---------------------------------------------------------
-// authentication (no middleware necessary since this isnt authenticated)
-// ---------------------------------------------------------
-// http://localhost:8080/api/authenticate
-apiRoutes.post('/authenticate', userController.signin);
-
-// ---------------------------------------------------------
-// route middleware to authenticate and check token
-// ---------------------------------------------------------
-apiRoutes.use(function(req, res, next) {
-
-	// check header or url parameters or post parameters for token
-	var token = req.body.token || req.param('token') || req.headers['x-access-token'];
-
-	// decode token
-	if (token) {
-
-		// verifies secret and checks exp
-		jwt.verify(token, app.get('superSecret'), function(err, decoded) {			
-			if (err) {
-				return res.json({ success: false, message: 'Failed to authenticate token.' });		
-			} else {
-				// if everything is good, save to request for use in other routes
-				req.decoded = decoded;	
-				next();
-			}
-		});
-
-	} else {
-
-		// if there is no token
-		// return an error
-		return res.status(403).send({ 
-			success: false, 
-			message: 'No token provided.'
-		});
-		
-	}
-	
-});
-
-// ---------------------------------------------------------
-// authenticated routes
-// ---------------------------------------------------------
-
-apiRoutes.get('/', function(req, res) {
-	res.json({ message: 'Welcome to the coolest API on earth!' });
-});
-
-apiRoutes.get('/users', function(req, res) {
-	User.find({}, function(err, users) {
-		res.json(users);
-	});
-});
-
-apiRoutes.get('/check', function(req, res) {
-	res.json(req.decoded);
-});
-
-app.use('/api', apiRoutes);
+require('./app/routes.js')(app, express); // load our routes and pass in our app and fully configured passport
 
 // =================================================================
 // start the server ================================================
